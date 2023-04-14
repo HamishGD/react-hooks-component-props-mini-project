e81ef3c0d62a720f14c9c069fde14b2671af9530
hamish <hamishgeorgedavie@gmail.com>
2023-04-14 10:54:44 +1000

    React hooks mini project

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+function About ({image = "https://via.placeholder.com/215Links to an external site", about}) {
+    return (
+        <aside>
+            <img src={image} alt="blog logo" />
+            <p>{about}</p>
+        </aside>
+    );
+}
+
+export default About;
+
---
+++ src/components/Article.js
@@ -0,0 +1,12 @@
+
+function Article({minutes, title, date="Janurary 1, 1970, preview"}) {
+    return (
+        <article>
+            <h3>{title}</h3>
+            <small>{date}</small>
+                <p>{preview}</p>
+        </article>
+    );
+}
+
+export default Article;
